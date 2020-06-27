b0c3ed15112de6c927fbef82ca5969de214660c5
omkar Kesarkhane <omkesarkhane@gmail.com>
2020-06-27 16:31:55 +0530

    Updates

---
+++ precache-manifest.2ae0e1a22c0b67421aeafb79e41968e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2b5f221be36c66c38016abef3fc8f2",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "17b507248707dc383dda",
+    "url": "/weather-forecast/static/css/main.dcf9e9e8.chunk.css"
+  },
+  {
+    "revision": "60e39db82c61552bcf07",
+    "url": "/weather-forecast/static/js/2.7f950c86.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/weather-forecast/static/js/2.7f950c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b507248707dc383dda",
+    "url": "/weather-forecast/static/js/main.6cd48189.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  },
+  {
+    "revision": "930eb6f4d75bea47f4f067dcc42ea56f",
+    "url": "/weather-forecast/static/media/Clouds.930eb6f4.jpg"
+  }
+]);
